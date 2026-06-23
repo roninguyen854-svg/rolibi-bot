5335d1fda7f00ec3df16fedbccf8e6e263bd6d86
roninguyen854-svg <roninguyen854@gmail.com>
2026-06-23 18:38:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const { Client, GatewayIntentBits, Events } = require('discord.js');
-
-const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
-});
-
-client.once(Events.ClientReady, () => {
-  console.log(`Logged in as ${client.user.tag}`);
-});
-
 client.on(Events.InteractionCreate, async interaction => {
   if (!interaction.isChatInputCommand()) return;
 
   if (interaction.commandName === 'ping') {
     await interaction.reply('🏓 Pong!');
   }
-});
 
-client.login(process.env.TOKEN);
+  if (interaction.commandName === 'balance') {
+    await interaction.reply('💰 Bạn có 1000 coins!');
+  }
+});
+new SlashCommandBuilder()
+  .setName('balance')
+  .setDescription('Xem số coin');
+git add .
+git commit -m "add balance"
+git push
